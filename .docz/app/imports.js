35d4b0af5ac6a0d295cd1d52766b58b07ac98633
Dan Burzo <danburzo@gmail.com>
2018-06-15 15:36:14 +0300

    add .docz to .gitignore

--- .docz/app/imports.js
+++
@@ -1,8 +0,0 @@
-export const imports = {
-  'docs/components/Input.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-components-input" */ 'docs/components/Input.mdx'),
-  'docs/components/List.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-components-list" */ 'docs/components/List.mdx'),
-  'docs/components/MultiSlider.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-components-multi-slider" */ 'docs/components/MultiSlider.mdx'),
-}
