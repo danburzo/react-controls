80409f75ba9703529c13cc42867a89f68668f04a
Dan Burzo <danburzo@gmail.com>
2018-11-28 19:23:20 +0200

    Remove react-event-listener

--- src/Husk/Husk.js
+++ src/Husk/Husk.js
@@ -4,13 +4,14 @@ import { func } from 'prop-types';
 class Husk extends React.Component {
 	componentDidMount() {
 		if (this.props.useEffect) {
-			this.on_unmount = this.props.useEffect();
+			this.__cleanup = this.props.useEffect();
 		}
 	}
 
 	componentWillUnmount() {
-		if (this.on_unmount) {
-			this.on_unmount();
+		if (typeof this.__cleanup === 'function') {
+			this.__cleanup();
+			this.__cleanup = null;
 		}
 	}
 
